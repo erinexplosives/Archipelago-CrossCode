44a953e26635e11a63f4a5ea4fc05cf3e9e3f84b
Richard Snider <lighthousemaniac@gmail.com>
2023-08-07 23:26:29 -0600

    Add game information to make tests not mad at me anymore

--- worlds/crosscode/__init__.py
+++ worlds/crosscode/__init__.py
@@ -12,6 +12,11 @@ class CrossCodeWebWorld(WebWorld):
     theme="ocean"
 
 class CrossCodeWorld(World):
+    """CrossCode is a retro-inspired 2D Action RPG set in the distant future,
+    combining 16-bit SNES-style graphics with butter-smooth physics, a
+    fast-paced combat system, and engaging puzzle mechanics, served with a
+    gripping sci-fi story.
+    """
     game = NAME
     web = CrossCodeWebWorld()
 
