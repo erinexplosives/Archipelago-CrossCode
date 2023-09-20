94ed72ed589e00dc60487c80dfa15cddbd62fe67
Richard Snider <lighthousemaniac@gmail.com>
2023-09-19 19:43:10 -0600

    add more tests to ensure better safety

--- worlds/crosscode/test/TestGeneratedCode.py
+++ worlds/crosscode/test/TestGeneratedCode.py
@@ -14,47 +14,6 @@ class TestLocationAttributes(CrossCodeTestBase):
             for data in locations_data:
                 assert(data.region[mode] in all_regions)
 
-class TestConditionsValid(CrossCodeTestBase):
-    auto_construct = False
-    locations_dict = {location.name: location for location in [*locations_data, *events_data]}
-
-    def test_item_conditions_on_locations_exist(self):
-        for data in locations_data:
-            for item, amount in data.cond.items:
-                item_name = item if amount == 1 else f"{item} x{amount}"
-                assert(item_name in items_dict)
-
-    def test_location_conditions_on_locations_exist(self):
-        for data in locations_data:
-            for location in data.cond.locations:
-                assert(location in self.locations_dict)
-
-    def test_region_conditions_on_locations_exist(self):
-        for data in locations_data:
-            for mode, regions in data.cond.regions.items():
-                for region in regions:
-                    assert(region in region_packs[mode].region_list)
-
-    def test_item_conditions_on_regions_exist(self):
-        for mode in modes:
-            for connection in region_packs[mode].region_connections:
-                for item, _ in connection.cond.items:
-                    assert(item in items_dict)
-
-    def test_location_conditions_on_regions_exist(self):
-        for mode in modes:
-            for connection in region_packs[mode].region_connections:
-                for location in connection.cond.locations:
-                    assert(location in self.locations_dict)
-
-    def test_region_conditions_on_regions_exist(self):
-        for mode in modes:
-            for connection in region_packs[mode].region_connections:
-                if mode not in connection.cond.regions:
-                    continue
-                for region in connection.cond.regions[mode]:
-                    assert(region in region_packs[mode].region_list)
-
 
 class TestItemQuantities(CrossCodeTestBase):
     auto_construct = False
