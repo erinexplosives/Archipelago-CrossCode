599609c2325bceeb816179e59c024ecf76e2510f
Richard Snider <lighthousemaniac@gmail.com>
2023-10-02 10:14:00 -0600

    regions in the new codegen

--- worlds/crosscode/templates/Regions.template.py
+++ worlds/crosscode/templates/Regions.template.py
@@ -8,17 +8,13 @@ modes = [ {{ modes_string }} ]
 default_mode = "{{ default_mode }}"
 
 region_packs: typing.Dict[str, RegionsData] = {
-    {% for r in region_packs -%}
-    "{{r.name}}": RegionsData(
+    {% for mode, r in region_packs.items() -%}
+    "{{mode}}": RegionsData(
         starting_region = "{{r.starting_region}}",
         goal_region = "{{r.goal_region}}",
         excluded_regions = {{r.excluded_regions}},
-        region_list = [
-            {{r.region_list | indent(12)}}
-        ],
-        region_connections = [
-            {{r.region_connections | indent(12)}}
-        ]
+        region_list = {{region_lists[mode] | indent(8)}},
+        region_connections = {{region_connections[mode] | indent(8)}}
     ),
     {% endfor %}
 }
